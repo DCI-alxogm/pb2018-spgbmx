9071b89019c3866126e0c1d035877fe3d1e486f6
Godinez Brizuela Salma Patricia <godinezbs2017@dci25-160.ugto.mx>
2018-08-08 09:25:39 -0500

    Cree mi primer programa de este curso.

---
+++ semana1/programa_hola.c
@@ -0,0 +1,14 @@
+/*
+Esto es un comentario.
+creado el 6/08/18 por Patricia Godinez 
+*/ 
+
+
+#include <stdio.h>
+int main (void)
+{
+	printf("Hola mundo \n");
+	printf("Este es el primer programa del curso, pero no de mi vida :¨v\n");
+
+}
+//Terminé de escribir el programa
