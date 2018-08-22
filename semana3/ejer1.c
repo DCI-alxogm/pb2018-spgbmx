968d422da9d4be0132ea3bc88676fefd8870a13d
Godinez Brizuela Salma Patricia <godinezbs2017@dci25-160.ugto.mx>
2018-08-22 09:21:22 -0500

    Actualizado

---
+++ semana3/ejer1.c
@@ -0,0 +1,14 @@
+//Este programa nos muestra cómo se utiliza la funcion de switch
+//Creado por Patricia Godinez el 22 de agosto de 2018
+#include<stdio.h>
+
+int main()
+{
+	int opcion;
+	printf("Elige uno de los siguientes comandos\n");
+	printf("(1) Si eres Hombre \t");
+	printf("(2) Si eres Mujer \t");
+
+
+
+}
