16d1431b71e749047cdb0da4f9c961bda741fa9b
Godinez Brizuela Salma Patricia <godinezbs2017@dci25-160.ugto.mx>
2018-09-10 09:22:24 -0500

    Semana 6 actualizada.

---
+++ semana6/ejem123.c
@@ -0,0 +1,25 @@
+//Este programa muestra los ejemplos de arreglos dados en clase
+//Escrito por Patricia Godinez el 10 de septiembre de 2018.
+#include<stdio.h>
+
+int main ()
+{
+	int i, j, N;
+	float numeros[i];
+
+	for(i=0;i<N;i++)
+	{
+		numeros[i]=0;//Se le asigna el valor de cero a todos los elementos del arreglo
+	}
+	for(i=0;i<N;i++)
+	{
+		scanf("%f",&numeros[i]);//Asigna valores a cada elemento del arreglo
+	}
+	for(j=0;j<N;j++)
+	{
+		printf("%f\n",numeros[i]);//Imprime todos los valores del arreglo
+		
+	}
+return 0;
+
+}
---
+++ semana6/ejem4.c
@@ -0,0 +1,16 @@
+//Este programa multiplica un numero dado por 2, haciendo uso de un arreglo
+//Escrito por Patricia Godinez el 10 de septiembre de 2018
+#include<stdio.h>
+
+int main ()
+{
+	int i, N;
+	float numeros[i];
+
+	for(i=0;i<N;i++)
+	{
+		scanf("%f",&numeros[i]);//Asigna el valor al arreglo.
+		numeros[i]*=2;//Multiplicación del arreglo por 2
+		printf("%f\n",numeros[i]);//imprime al arreglo modificado por la multiplicacion
+	}
+}
