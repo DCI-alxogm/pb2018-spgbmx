6bfdb171d43e39dd82804e0d88662b43c49680a8
Godinez Brizuela Salma Patricia <godinezbs2017@dci25-160.ugto.mx>
2018-09-17 09:47:42 -0500

    Actualizado.

---
+++ semana7/inte.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main ()
+{
+	int p, a, b, N, K;
+	float fx, Ix
+		
+	//Leyendo el archivo.
+	FILE *datos;
+	datos=fopen("entrada.txt","r");
+	fscanf(datos,"%f %f %f %f %f",&p,&a,&b,&N,&K);
+
+	//Realizando los cálculos.
+	
+}
